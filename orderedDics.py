c7652e2685fee7f2ed12214b43565c78bc581c25
bjdumm <bjdumm45@gmail.com>
2022-07-22 15:38:46 -0400

    Started adding Ordered words

---
+++ orderedDics.py
@@ -0,0 +1,27 @@
+import pickle
+from utils import loadPickle,dumpPickle
+
+
+"""
+#Here are your ordered arrays of English keys
+past = loadPickle("Sections/Past.txt")
+print("Section in order" , list(past.keys()))
+
+conditional = loadPickle("Sections/Conditional.txt")
+print("Section in order" , list(conditional.keys()))
+
+perfect = loadPickle("Sections/Perfect.txt")
+print("Section in order" , list(perfect.keys()))
+
+future = loadPickle("Sections/Future.txt")
+print("Section in order" , list(future.keys()))
+
+present = loadPickle("Sections/Present.txt")
+print("Section in order" , list(present.keys()))
+
+progressive = loadPickle("Sections/Progressive.txt")
+print("Section in order" , list(progressive.keys()))
+
+toBe = loadPickle("Sections/To Be & To Have.txt")
+print("Section in order" , list(toBe.keys()))
+"""
